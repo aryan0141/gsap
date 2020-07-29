3e0295039e64c951bdf5a68ce315d22ec51bf6ab
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2020-07-29 12:55:23 +0530

    Some little changes in x-dimension of box

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ pause.onclick = () => tween.pause();
 reverse.onclick = () => tween.reverse();
 kill.onclick = () => tween.kill();
 
-gsap.to(".box", { rotation: 27, duration: 2, yoyo: true, repeat: -1, ease: 'sine', x: 1000 })
+gsap.to(".box", { rotation: 27, duration: 2, yoyo: true, repeat: -1, ease: 'sine', x: innerWidth-400 })
